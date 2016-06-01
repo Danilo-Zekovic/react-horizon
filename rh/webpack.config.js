6ad8bea4ced77847b3e00d049e79dc584c5e4ffe
Danilo Zekovic <danilozeka93@gmail.com>
2016-06-01 13:30:37 -0400

    simple webpack config

--- rh/webpack.config.js
+++ rh/webpack.config.js
@@ -6,7 +6,14 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
+            {
+              test: /.jsx?$/,
+              loader: 'babel-loader',
+              exclude: /node_modules/,
+              query: {
+                presets: ['es2015', 'react']
+              }
+            }
         ]
     }
 };
