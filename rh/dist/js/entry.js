68ed1b59ed77e47bdaf6a25fa97039cdfa445e30
Danilo Zekovic <danilozeka93@gmail.com>
2016-06-08 00:15:06 -0400

    shell

--- rh/dist/js/entry.js
+++ rh/dist/js/entry.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router'
 
 import Hello from './hello.jsx';
 import Header from './Header.jsx'
 
+import App from './App'
+import About from './About'
+import Repos from './Repos'
+import Repo from './Repo'
+import { Router, Route, hashHistory, browserHistory, IndexRoute } from 'react-router'
+import Home from './Home'
 
 var horizon = Horizon()
 horizon.onReady(function() {
   ReactDOM.render((
-      <div>
-        <Header/>
-        <Hello/>
-
-      </div>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <Route path="/repos" component={Repos}>
+          <Route path="/repos/:userName/:repoName" component={Repo}/>
+        </Route>
+        <Route path="/about" component={About}/>
+      </Route>
+    </Router>
     )
     , document.getElementById('main')
   );
